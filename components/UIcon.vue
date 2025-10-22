1b9976335ec5fbe0e7ad0f2ab554c996392bfe72
Nomikuza <uzumakisujai@gmail.com>
2025-10-22 18:25:00 +0700

    Add stub UIcon component to override Nuxt UI and prevent recursion

---
+++ components/UIcon.vue
@@ -0,0 +1,13 @@
+<template>
+  <!-- Stub component to prevent Nuxt UI icon recursion -->
+  <span></span>
+</template>
+
+<script setup>
+// This component overrides Nuxt UI's UIcon to prevent recursion
+// All icons are now handled by Iconify in the navbar
+defineProps({
+  name: String
+})
+</script>
+
