21ddf06de1aab688023fa8b2c91aaedc469d9616
Nomikuza <uzumakisujai@gmail.com>
2025-10-22 13:55:22 +0700

    fix: replace Font Awesome icons with Lucide icons in Area and MikroTik pages

--- pages/dashboard/area/index.vue
+++ pages/dashboard/area/index.vue
@@ -104,7 +104,7 @@ async function deleteData(id: string){
 
 <template>
   <div class="flex gap-2">
-    <UButton icon="fa-plus" color="blue" label="Add New Area" @click="openModal(false,null)" />
+    <UButton icon="i-lucide-plus" color="blue" label="Add New Area" @click="openModal(false,null)" />
   </div>
   <div class="flex px-3 py-3.5 border-b border-gray-200 dark:border-gray-700">
     <UInput v-model="q" placeholder="Filter Area..." />
@@ -118,8 +118,8 @@ async function deleteData(id: string){
     </template>
     <template #actions-data="{ row }">
       <div class="flex gap-2">
-        <UButton icon="fa-pencil" color="yellow" label="Edit" @click="openModal(true,row)" />
-        <UButton icon="fa-trash" color="red" label="Delete" @click="deleteData(row.id)" />
+        <UButton icon="i-lucide-pencil" color="yellow" label="Edit" @click="openModal(true,row)" />
+        <UButton icon="i-lucide-trash-2" color="red" label="Delete" @click="deleteData(row.id)" />
       </div>
     </template>
   </UTable>
